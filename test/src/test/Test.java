d051687f1af8dad20f919971c879cf25af98424d
hglps <44714747+hglps@users.noreply.github.com>
2021-06-04 23:36:27 -0300

    Add files via upload

---
+++ test/src/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Hiago
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main() {
+        // TODO code application logic here
+        System.out.println("aaaaaa");
+        Object[] str = {"a", "ab", "abc"};
+        System.out.println(str[1]);
+    }
+    
+}
